0c5a3331f71a16ddd22934652999c3c69741b926
arash-ash <arash.asn94@gmail.com>
2016-06-12 22:18:48 +0300

    Initial commit - v0.2

---
+++ app/src/main/java/edu/bilkent/findatutor/fragment/MyPostsFragment.java
@@ -0,0 +1,18 @@
+package edu.bilkent.findatutor.fragment;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.Query;
+
+public class MyPostsFragment extends PostListFragment {
+
+    public MyPostsFragment() {
+
+    }
+
+    @Override
+    public Query getQuery(DatabaseReference databaseReference) {
+        // All my posts
+        return databaseReference.child("user-posts")
+                .child(getUid());
+    }
+}
